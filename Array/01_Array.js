ca6b9c61e318b2b499c326f30706d93832cb331b
Surya Prakash <suryasharma122019@gmail.com>
2026-06-06 21:12:56 +0530

    array

---
+++ Array/01_Array.js
@@ -0,0 +1,31 @@
+
+
+let myArr = [1, 2, 3, 4, 5]
+
+// console.log(myArr[1]);
+
+// myArr.push(6)
+// myArr.pop()
+
+// myArr.unshift(6)
+// myArr.shift(4)
+
+
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(1));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+const n1 = myArr.slice(1, 3)
+
+console.log(" A " ,myArr);
+console.log(n1);
+
+
+const n2 = myArr.splice(1,3)
+console.log(" B ",myArr);
+
+console.log(n2);
---
+++ Basics/dateinJs.js
@@ -0,0 +1,18 @@
+
+// let myDate = new Date()
+
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+
+// console.log(typeof myDate);
+
+let createdDate = new Date(2026, 0, 10, 6, 30)
+console.log(createdDate.toLocaleString());
+
+
+
+
